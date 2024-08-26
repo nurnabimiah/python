2361d1954f66cbe68a961bbf5ad920b8ac5745b7
md.nurnabimiah <diunayonahmed@gmail.com>
2024-08-26 10:49:08 +0600

    start print  : w3 resources-conditional statement -(9) problem solved

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+n = int(input('Enter the input number:  '))
+d1, d2 = 0, 1
+print('Fibo series: ', d1, d2,end= ' ')
+
+# loop
+for i in range(2, n):
+    fibo_number = d1 + d2
+    print(fibo_number, end=" ")
+    d1 = d2
+    d2 = fibo_number
 
 
-input_word = input("Take a word which you want to the reverse: ")
-word_length = len(input_word)
-reverse_word = ""
-for index in range(word_length-1,-1,-1):
-    reverse_word = reverse_word + input_word[index]
-print("Reverse word : " + reverse_word)
+
+
