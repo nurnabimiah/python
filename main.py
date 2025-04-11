882db098000c37e9557548b4d02e633b994fcc6a
md.nurnabimiah <diunayonahmed@gmail.com>
2025-04-11 23:02:32 +0600

    fibonacci series  : w3 resources-conditional statement -(9) problem not solved

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-n = int(input('Enter the input number:  '))
-d1, d2 = 0, 1
-print('Fibo series: ', d1, d2,end= ' ')
 
-#   loop for fibo
-for i in range(2, n):
-    fibo_number = d1 + d2
-    print(fibo_number, end=" ")
-    d1 = d2
-    d2 = fibo_number
+
+print('Hello fibonacci')
+
+
+
 
 
 
